391567118be272361879e80df48988a4ae9b8205
Alexandre Sanchez <alex73630@gmail.com>
2017-05-25 06:11:40 +0200

    Done a lot of changes
    Added some .gitignore lines
    config.json.sample changed to have plex folders locations values inside.
    Done some cleaning on index.js.
    Now using incremental setTimeout for requests on each video posts.
    Json for videos changed a little bit.
    Started to work on the download system. Still broken...

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var request = require('request');
 var fs = require('fs');
 var cheerio = require('cheerio');
 var sleep = require('sleep');
+var https = require('https');
 var config = require('./config.json');
 
 // Create cookie container
@@ -10,10 +11,6 @@ var cookiejar = request.jar();
 
 // Set forum login url
 var loginURL = "https://linustechtips.com/main/login/"
-
-// Store forum page
-
-var floatplanePage
 
 // Do a first get so we have some values/cookies to do the login
 request({url: loginURL,jar: cookiejar}, function (error, response, body) {
@@ -44,75 +41,97 @@ request({url: loginURL,jar: cookiejar}, function (error, response, body) {
 			console.log('error:', error);
 			console.log('statusCode:', response && response.statusCode);
 
-			// Get Floatplane Club first page and parse it
-			request({url: 'https://linustechtips.com/main/forum/91-the-floatplane-club/',jar: cookiejar},
-			function (error, response, body) {
-				console.log('error:', error); // Print the error if one occurred
-				console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-				floatplanePage = ''+ body;
-				var $ = cheerio.load(floatplanePage);
-				var baseURLtarget = 'https://linustechtips.com/main/forum/91-the-floatplane-club/?page=1';
-				var postList = $('div.ipsBox[data-baseurl="' + baseURLtarget + '"]').children('ol');
-				var postTitles = postList.children().children('div.ipsDataItem_main');
-				var postTitleContainer = postTitles.children('h4').children('div').children('a');
-				var postTimeContainer = postTitles.children('div').children('time');
+			// GET FLOATPLANE CLUB FIRST PAGE HERE
+			getFloatplanePage();
+});
+})
 
-				var linksAndTitles = []
+// Store forum page
+var floatplanePage
 
-				var posts = []
-				// List 24 last posts
-				for (let i = 1; i < 25; i++) {
+var interval = 1 * 500;
+// Get Floatplane Club first page and parse it
+function getFloatplanePage() {
+	request({url: 'https://linustechtips.com/main/forum/91-the-floatplane-club/',jar: cookiejar},
+		function (error, response, body) {
+			console.log('error:', error); // Print the error if one occurred
+			console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+
+			floatplanePage = ''+ body;
+			var $ = cheerio.load(floatplanePage);
+
+			var baseURLtarget = 'https://linustechtips.com/main/forum/91-the-floatplane-club/?page=1';
+			var postList = $('div.ipsBox[data-baseurl="' + baseURLtarget + '"]').children('ol');
+			var postTitles = postList.children().children('div.ipsDataItem_main');
+			var postTitleContainer = postTitles.children('h4').children('div').children('a');
+			var postTimeContainer = postTitles.children('div').children('time');
+
+			var linksAndTitles = []
+
+			// List 24 last posts
+			for (let i = 1; i < 25; i++) {
+				setTimeout(function() {
+					console.log('Start requesting:', postTitleContainer[i].attribs.href);
 					request({url: postTitleContainer[i].attribs.href, jar: cookiejar}, function (error, response, body) {
 						var $ = cheerio.load(body);
 						sleep.msleep(200);
+
 						// Get all values and save them in a json
 						videoID = $('.floatplane-script').data('videoGuid');
 
 						title = postTitleContainer[i].attribs.title;
 						regType = new RegExp(/\w+(?=:)/);
 						function parsingType(){
-							if (regType.exec(title) === null || regType.exec(title) === undefined){return ''}
+							if (regType.exec(title) === null || regType.exec(title) === undefined){return 'Unknown'}
 							else {
-								return regType.exec(title);
+								return regType.exec(title)[0];
 							}}
 						parsedType = parsingType();
+
+						regTitle = new RegExp(/(?: ).+/);
+						parsedTitle = regTitle.exec(title)[0].slice(1);
 
 						postURL = postTitleContainer[i].attribs.href;
 
 						dateTime = postTimeContainer[i].attribs.datetime;
 						regDate = new RegExp(/\d+-\d+-\d+/);
-						parsedDate = regDate.exec(dateTime);
+						parsedDate = regDate.exec(dateTime)[0];
+
 						console.log('---------------------');
 						console.log('error:', error); // Print the error if one occurred
 						console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
 						console.log('*********');
 						console.log('videoID:',videoID);
-						console.log('title:',title);
-						console.log('postURL:',postURL);
+						console.log('type:',parsedType);
+						console.log('title:',parsedTitle);
+						console.log('date:',parsedDate);
+
 						linksAndTitles[i] = {
 							postID:i,
-							title:title,
-							type:parsedType[0], // If it's a LTT or CSF or TQ video
+							title:parsedTitle,
+							type:parsedType, // If it's a LTT or CSF or TQ video
 							serie:'Scrapyard Wars',
 							season: 5,
 							episode: 3,
 							postURL:postURL,
-							date:parsedDate[0],
+							date:parsedDate,
 							videoID:videoID,
 							dlURL:''}
-						if (videoID !== undefined) {
+
+						if (videoID !== undefined && parsedType !== 'Unknown') {
 							videoQuality = config.videoQuality;
-							getDlUrl = 'https://linustechtips.com/main/applications/floatplane/interface/video_url.php?video_guid=' + videoID + '&video_quality='+ videoQuality +'&download=1'
-							existInNew = fs.existsSync('json/new/' + linksAndTitles[i].videoID + '.json');
-							existInCompleted = fs.existsSync('json/completed/' + linksAndTitles[i].videoID + '.json');
+							getDlUrl = 'https://linustechtips.com/main/applications/floatplane/interface/video_url.php?video_guid=' + videoID + '&video_quality='+ videoQuality +'&download=1';
+							//filename =  linksAndTitles[i].type + '-' + linksAndTitles[i].date + '-' + linksAndTitles[i].videoID + '.json';
+
+							// Create vars to test if a json already exist
+							existInNew = fs.existsSync('json/new/' + linksAndTitles[i].type + '-' + linksAndTitles[i].date + '-' + linksAndTitles[i].videoID + '.json');
+							existInCompleted = fs.existsSync('json/completed/' + linksAndTitles[i].type + '-' + linksAndTitles[i].date + '-' + linksAndTitles[i].videoID + '.json');
 
 							if (existInNew === false && existInCompleted === false) {
 								console.log(linksAndTitles[i].videoID,'File do not exist!');
 								request({url: getDlUrl, jar:cookiejar}, function (error, response, body) {
-									//console.log(postTitleContainer[i].attribs.title + ' DL URL:', body);
 									linksAndTitles[i].dlURL = body;
-									filename = 'json/new/' + linksAndTitles[i].title + '.json'
-									fs.writeFileSync('json/new/' + linksAndTitles[i].videoID + '.json', JSON.stringify(linksAndTitles[i]));
+									fs.writeFileSync('json/new/' + linksAndTitles[i].type + '-' + linksAndTitles[i].date + '-' + linksAndTitles[i].videoID + '.json', JSON.stringify(linksAndTitles[i]));
 								})
 							}
 							else {
@@ -120,12 +139,57 @@ request({url: loginURL,jar: cookiejar}, function (error, response, body) {
 							}
 						}
 					})
-				}
-
+				}, interval * i, i);
 			}
-		);
-});
-})
+			setTimeout(function(){
+				downloadVideos();
+			}, interval * 30);
+		}
+	);
+}
+
+function parseTypeForTitle(type){
+	if (type == "LTT") {
+		return "Linus Tech Tips"
+	} else if (type == "CSF") {
+		return "Channel Super Fun"
+	} else if (type == "TQ") {
+		return "Techquickie"
+	}
+}
+function videoTypeFolderFun(type) {
+	if (type == "LTT") {
+		return config.lttFolderName
+	} else if (type == "CSF") {
+		return config.csfFolderName
+	} else if (type == "TQ") {
+		return config.tqFolderName
+	}
+}
+
+function downloadVideos() {
+	var jsonReg = new RegExp(/(.json)/);
+	jsonNewDir = 'json/new/';
+	jsonCompletedDir = 'json/completed/'
+	fs.readdir(jsonNewDir, function(err, files){
+		files.forEach(function(file){
+			if (jsonReg.test(file) === true) {
+				console.log(file,'is a json');
+				fileJson = JSON.parse(fs.readFileSync(jsonNewDir + file));
+				console.log(file,'download url:',fileJson.dlURL);
+
+				parsedTypeForTitle = parseTypeForTitle(fileJson.type);
+				videoTypeFolder = videoTypeFolderFun(fileJson.type);
+
+				request(fileJson.dlURL)
+				.on('response', function (res) {
+					res.pipe(fs.createWriteStream(config.plexFolder + videoTypeFolder + parsedTypeForTitle + ' - ' + fileJson.date + ' - ' + fileJson.title + '.mp4'))
+				})
+				.on('finish', function(){fs.renameSync(jsonNewDir + file, jsonCompletedDir + file);});
+			}
+		})
+	})
+};
 
 /* SIDENOTE
 I should rewrite a huge part of this code and use more "callback"/"function calling"
